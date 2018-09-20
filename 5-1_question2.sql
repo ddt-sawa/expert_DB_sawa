1ec8944c3d18f8c7105552f8274cd3a9b0a19203
ddt-sawa <ddt.sawa@gmail.com>
2018-09-20 19:29:12 +0900

    add text, Chapter5

---
+++ 5-1_question2.sql
@@ -0,0 +1,76 @@
+﻿/*5-1 問2 支社/支店別の取り扱い商品の一覧。結果には支社名・支店名・商品名を含むものとする。
+（PostgreSQLを使用します)*/
+
+--取得列
+SELECT
+
+	--支社名
+	BranchCompany.BRANCH_COMPANY_NAME,
+	
+	--支店名
+	BranchShop.BRANCH_NAME,
+	
+	--商品名
+	Product.PRODUCT_NAME
+
+--参照範囲
+FROM
+
+	--支社テーブルと支店テーブルを結合する
+	( 
+		--それらと支店商品テーブルを結合する
+		(
+			--それらと商品テーブルを結合する
+			(
+				--支社テーブル
+				BranchCompany
+	
+			--NULL値を考慮しないので内部結合
+			INNER JOIN
+	
+				--支店テーブル
+				BranchShop
+	
+			--結合列
+			ON
+	
+				--支社コード列
+				BranchCompany.BRANCH_COMPANY_ID = BranchShop.BRANCH_COMPANY_ID
+		
+			--支社テーブルと支社テーブル結合完了、次は支店商品テーブルを結合
+			)
+			
+	--NULL値を考慮しないので内部結合
+	INNER JOIN
+	
+		--支店商品テーブル
+		CompanyShopProduct
+		
+	--結合列
+	ON
+	
+		--支社コード列
+		CompanyShopProduct.BRANCH_COMPANY_ID = BranchCompany.BRANCH_COMPANY_ID
+		
+	--と	
+	AND
+		
+		--支店コード列
+		CompanyShopProduct.BRANCH_SHOP_ID = BranchShop.BRANCH_SHOP_ID
+		
+		--支店商品テーブル結合終了、次は商品テーブルを結合
+		)
+		
+	--NULL値を考慮しないので内部結合
+	INNER JOIN
+	
+		--商品テーブル
+		Product
+		
+	--結合列
+	ON
+	
+		--商品コード列
+		CompanyShopProduct.PRODUCT_ID = Product.PRODUCT_ID
+	)
+;
---
+++ 5-2_question3.sql
@@ -0,0 +1,46 @@
+﻿/*5-2 問3の非正規化によるSQLチューニング。
+（PostgreSQLを使用します)*/
+
+--チューニング方法:支店テーブルに商品数(PRODUCT_NUMBER)列を追加する
+--以下、チューニングを行った際のSQL文
+
+--取得列
+SELECT
+
+	--支社コード
+	BranchShop.BRANCH_COMPANY_ID,
+	
+	--支店コード
+	BranchShop.BRANCH_SHOP_ID,
+	
+	--商品数
+	BranchShop.PRODUCT_NUMBER
+	
+--参照範囲
+FROM
+
+	--支店テーブル
+	BranchShop
+	
+--取得条件
+WHERE
+
+ 	--商品数が最大
+	PRODUCT_NUMBER = 
+	
+	--最大商品数を求めるサブクエリ
+	(
+	
+	--取得値
+	SELECT 
+	
+		--最大商品数
+		MAX(BranchShop.PRODUCT_NUMBER)
+		
+	--参照範囲
+	FROM
+
+		--支店テーブル
+		BranchShop
+	)
+;
