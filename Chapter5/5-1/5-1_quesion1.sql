98d2ab2b04245feb2192271394de00ec5c827e9a
ddt-sawa <ddt.sawa@gmail.com>
2018-09-23 00:31:46 +0900

    mod text, Chapter5 フォルダ化

---
+++ Chapter5/5-1/5-1_quesion1.sql
@@ -0,0 +1,43 @@
+﻿/*5-1 問1 商品分類ごとの商品数。結果には分類名を含むものとする。
+（PostgreSQLを使用します)*/
+
+
+--商品分類ごとの商品数を把握するため、商品分類コードとその分類名、商品分類コードに関連する商品コードの列数を取得する
+SELECT
+
+	--商品分類
+	ProductClassification.PRODUCT_CLASSIFICATION_ID, 
+
+	--商品分類名
+	ProductClassification.PRODUCT_CLASSIFICATION_NAME,
+	
+	--商品分類ごとの商品数
+	Count(Product.PRODUCT_ID)
+
+--参照範囲
+FROM
+
+	--商品分類テーブル
+	ProductClassification
+	
+--商品分類コードと商品コードの関連が載っている商品テーブルについて、NULL値を考慮しないので内部結合
+INNER JOIN
+
+	--商品テーブル
+	Product
+	
+--結合列
+ON
+	--両テーブルにおける商品分類コード
+	 ProductClassification.PRODUCT_CLASSIFICATION_ID = Product.PRODUCT_CLASSIFICATION_ID
+	 
+	 
+--グループ化のキー値
+GROUP BY
+
+	--商品分類コード
+	ProductClassification.PRODUCT_CLASSIFICATION_ID,
+	
+	--商品分類名
+	ProductClassification.PRODUCT_CLASSIFICATION_NAME
+;
---
+++ Chapter5/5-1/5-1_question3.sql
@@ -0,0 +1,194 @@
+﻿/*5-1 問3 最も取り扱い商品数が多い支店の支店コードと商品数。
+（PostgreSQLを使用します)*/
+
+--取得列
+SELECT
+
+	--支社コード
+	CompanyShopProduct.BRANCH_COMPANY_ID,
+	
+	--支店コード
+	CompanyShopProduct.BRANCH_SHOP_ID,
+	
+	--商品数
+	COUNT(Product.PRODUCT_ID) AS 最大商品数
+	
+
+--参照範囲
+FROM
+
+	--支社テーブルと支店テーブルを結合する
+	( 
+		--それらと支店商品テーブルを結合する
+		(
+			--それらと商品テーブルを結合する
+			(
+				--支社テーブル
+				BranchCompany
+	
+			--NULL値を考慮しないので内部結合
+			INNER JOIN
+	
+				--支店テーブル
+				BranchShop
+	
+			--結合列
+			ON
+	
+				--支社コード列
+				BranchCompany.BRANCH_COMPANY_ID = BranchShop.BRANCH_COMPANY_ID
+		
+			--支社テーブルと支社テーブル結合完了、次は支店商品テーブルを結合
+			)
+			
+		--NULL値を考慮しないので内部結合
+		INNER JOIN
+	
+			--支店商品テーブル
+			CompanyShopProduct
+		
+		--結合列
+		ON
+		
+			--支社コード列
+			CompanyShopProduct.BRANCH_COMPANY_ID = BranchCompany.BRANCH_COMPANY_ID
+		
+		--と	
+		AND
+		
+			--支店コード列
+			CompanyShopProduct.BRANCH_SHOP_ID = BranchShop.BRANCH_SHOP_ID
+		
+		--支店商品テーブル結合終了、次は商品テーブルを結合
+		)
+		
+	--NULL値を考慮しないので内部結合
+	INNER JOIN
+	
+			--商品テーブル
+			Product
+		
+		--結合列
+		ON
+	
+			--商品コード列
+			CompanyShopProduct.PRODUCT_ID = Product.PRODUCT_ID
+	)
+
+--支店ごとにグループ化
+GROUP BY
+
+	--支社コード
+	CompanyShopProduct.BRANCH_COMPANY_ID,
+	
+	--支店コード
+	CompanyShopProduct.BRANCH_SHOP_ID
+	
+--グループ化後に課す取得条件
+HAVING 
+
+	--商品数が最大
+	COUNT(Product.PRODUCT_ID) = 
+	
+	--最大商品数を求めるサブクエリ
+	(
+		--取得値
+		SELECT
+		
+			--最大商品数
+			MAX (商品数)
+		
+		--参照範囲
+		FROM
+		
+		--最大商品数を求めるための参照範囲を求めるサブクエリ
+		(
+		
+		--取得列
+		SELECT
+	
+			--支社コード
+			CompanyShopProduct.BRANCH_COMPANY_ID,
+			
+			--支店コード
+			CompanyShopProduct.BRANCH_SHOP_ID,
+			
+			--商品数
+			COUNT(Product.PRODUCT_ID) AS 商品数
+	
+		--参照範囲
+		FROM
+	
+		--支社テーブルと支店テーブルを結合する
+		( 
+			--それらと支店商品テーブルを結合する
+			(
+				--それらと商品テーブルを結合する
+				(
+					--支社テーブル
+					BranchCompany
+		
+				--NULL値を考慮しないので内部結合
+				INNER JOIN
+		
+					--支店テーブル
+					BranchShop
+		
+				--結合列
+				ON
+		
+					--支社コード列
+					BranchCompany.BRANCH_COMPANY_ID = BranchShop.BRANCH_COMPANY_ID
+			
+				--支社テーブルと支社テーブル結合完了、次は支店商品テーブルを結合
+				)
+				
+			--NULL値を考慮しないので内部結合
+			INNER JOIN
+		
+				--支店商品テーブル
+				CompanyShopProduct
+			
+			--結合列
+			ON
+			
+				--支社コード列
+				CompanyShopProduct.BRANCH_COMPANY_ID = BranchCompany.BRANCH_COMPANY_ID
+			
+			--と	
+			AND
+			
+				--支店コード列
+				CompanyShopProduct.BRANCH_SHOP_ID = BranchShop.BRANCH_SHOP_ID
+			
+			--支店商品テーブル結合終了、次は商品テーブルを結合
+			)
+			
+		--NULL値を考慮しないので内部結合
+		INNER JOIN
+		
+				--商品テーブル
+				Product
+			
+			--結合列
+			ON
+		
+				--商品コード列
+				CompanyShopProduct.PRODUCT_ID = Product.PRODUCT_ID
+		)
+	
+		--支店ごとにグループ化
+		GROUP BY
+	
+			--支社コード
+			CompanyShopProduct.BRANCH_COMPANY_ID,
+		
+			--支店コード
+			CompanyShopProduct.BRANCH_SHOP_ID
+			
+		--最大商品数を求めるための参照範囲を求めるサブクエリ終了
+		) AS SubQuery
+
+	--最大商品数を求めるサブクエリ終了
+	)
+;
